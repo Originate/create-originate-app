d0c8048bf5527df0714849148e7d19eedbfcbded
Hao Lian <hi@haolian.org>
2020-08-25 13:05:48 -0400

    lib: bugfix(withDefault type)

--- template/packages/lib/io-ts.ts
+++ template/packages/lib/io-ts.ts
@@ -1,7 +1,7 @@
 import {isLeft} from 'fp-ts/lib/Either';
 import * as D from 'io-ts/lib/Decoder';
 
-export function withDefault<T>(defaultValue: T, decoder: D.Decoder<T, unknown>): D.Decoder<T, unknown> {
+export function withDefault<T, U>(defaultValue: U, decoder: D.Decoder<T, U>): D.Decoder<T, U> {
   return {
     decode: (u) => {
       const result = decoder.decode(u);
