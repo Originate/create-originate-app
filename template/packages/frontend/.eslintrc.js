e8336dd3009b8c9b083ff11d1082e2b624c92dd1
Jesse Hallett <jesse.hallett@originate.com>
2020-10-30 13:02:38 -0400

    more linting for graphql queries

--- template/packages/frontend/.eslintrc.js
+++ template/packages/frontend/.eslintrc.js
@@ -6,8 +6,16 @@ module.exports = {
   env: {
     node: true,
   },
-  plugins: ["ts-graphql"],
+  plugins: ["graphql", "ts-graphql"],
   rules: {
+    "graphql/capitalized-type-name": "warn",
+    "graphql/named-operations": "error",
+	"graphql/no-deprecated-fields": ["warn", { env: "apollo" }],
+    "graphql/required-fields": [
+      "error",
+      { env: "apollo", requiredFields: ["id"] },
+    ],
+    "graphql/template-strings": ["error", { env: "apollo" }],
     "ts-graphql/gql-type-assertion": "error",
   },
 }
