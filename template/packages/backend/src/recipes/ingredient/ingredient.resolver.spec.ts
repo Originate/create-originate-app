8b513c6a3d4df1eccd40e960e0ec4faa1d698f14
Jesse Hallett <jesse.hallett@originate.com>
2020-11-12 17:36:22 -0500

    test: get all backend tests into a working state; run tests in CI (#99)

--- template/packages/backend/src/recipes/ingredient/ingredient.resolver.spec.ts
+++ template/packages/backend/src/recipes/ingredient/ingredient.resolver.spec.ts
@@ -1,18 +1,54 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { IngredientResolver } from './ingredient.resolver';
+import { ContextIdFactory } from "@nestjs/core"
+import { Test, TestingModule } from "@nestjs/testing"
+import { Ingredient, IngredientCategory } from "../models/ingredient.entity"
+import { IngredientResolver } from "./ingredient.resolver"
+import { IngredientService } from "./ingredient.service"
 
-describe('IngredientResolver', () => {
-  let resolver: IngredientResolver;
+// Mock the service class so that we don't have to provide its dependencies.
+jest.mock("./ingredient.service")
+
+describe("IngredientResolver", () => {
+  let resolver: IngredientResolver
+  let service: IngredientService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [IngredientResolver],
-    }).compile();
+      providers: [IngredientResolver, IngredientService],
+    }).compile()
 
-    resolver = module.get<IngredientResolver>(IngredientResolver);
-  });
+    // Set up a context ID for request-scoped providers so we can get at
+    // a resolver instance, and its matching service instance.
+    const contextId = ContextIdFactory.create()
+    jest.spyOn(ContextIdFactory, "getByRequest").mockReturnValue(contextId)
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
-  });
-});
+    resolver = await module.resolve(IngredientResolver, contextId)
+    service = await module.resolve(IngredientService, contextId)
+  })
+
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  it("saves a new ingredient", async () => {
+    const ingredient: Ingredient = {
+      id: 1,
+      name: "raisins",
+      category: IngredientCategory.FRUIT,
+    }
+
+    jest.spyOn(service, "create").mockImplementation(async ingredientInput => {
+      expect(ingredientInput).toEqual({
+        name: "raisins",
+        category: IngredientCategory.FRUIT,
+      })
+      return ingredient
+    })
+
+    expect(
+      await resolver.addIngredient({
+        name: "raisins",
+        category: IngredientCategory.FRUIT,
+      }),
+    ).toEqual(ingredient)
+  })
+})
