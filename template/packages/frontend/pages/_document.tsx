1403010941f24c5332bfb7cc423ac65cdbc787c3
Jesse Hallett <jesse.hallett@originate.com>
2020-11-13 16:23:21 -0500

    server-side rendering for styled components
    The configuration is based on the example here:
    https://github.com/vercel/next.js/tree/master/examples/with-styled-components
    
    It's important to render a styled components stylesheet server-side
    because when the page first displayed React hasn't run - we're rendering
    server-generated HTML - so styled components won't inject styles
    dynamically until the first *re-render*.

---
+++ template/packages/frontend/pages/_document.tsx
@@ -0,0 +1,49 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document"
+import { ServerStyleSheet } from "styled-components"
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          // Analyze CSS from styled components in the page to be rendered to
+          // generate server-rendered stylesheet.
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
