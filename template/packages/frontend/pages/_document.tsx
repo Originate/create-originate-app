3903e97cc648d1aa99c9237b7fe4108cde34bdd3
Jesse Hallett <jesse.hallett@originate.com>
2020-11-13 16:34:23 -0500

    add some documentation comments to _document and _app

--- template/packages/frontend/pages/_document.tsx
+++ template/packages/frontend/pages/_document.tsx
@@ -7,7 +7,17 @@ import Document, {
 } from "next/document"
 import { ServerStyleSheet } from "styled-components"
 
-export default class MyDocument extends Document {
+/**
+ * This custom Document class customizes server-side rendering and generation.
+ * (Nothing here runs client side.) Customizations that should apply to every
+ * page can be made here, like setting a `lang="en"` attribute on the `<Html>`
+ * tag. But most <head> tag stuff, like setting a page title, should be handled
+ * in page components (not here) using the `Head` tag imported from "next/head"
+ * so that values can change during client-side page transitions.
+ *
+ * See https://nextjs.org/docs/advanced-features/custom-document
+ */
+export default class OriginateDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
