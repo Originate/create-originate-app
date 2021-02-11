0b7e6fb9a3a13de02e495a53479835318e305c40
xasopheno <danny@xasopheno.com>
2021-02-11 14:52:00 -0500

    WIP

--- src/helpers.ts
+++ src/helpers.ts
@@ -202,12 +202,7 @@ export function editBackendTopLevelJson(appName: string, targetDir: string) {
       "dev:frontend"
     ] = `yarn workspace @${appName}/frontend start:dev`
     json.scripts["dev:backend"] = `yarn workspace @${appName}/backend start:dev`
-    json.scripts["start:frontend"] = `yarn workspace @${appName}/frontend start`
-    json.scripts["start:backend"] = `yarn workspace @${appName}/backend start`
     json.scripts["build"] = `yarn workspaces run build`
-    json.scripts[
-      "db:run:migrations"
-    ] = `yarn workspace @${appName}/backend db:migration:run:prod`
 
     fs.writeFileSync(
       filename,
